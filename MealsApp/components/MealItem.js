b8e3d28fb889cdbb45971eca68aac1c797eb3378
Léo Schoofs <leo.schoofs5@gmail.com>
2026-01-29 15:51:57 +0100

    Section 6, 106: new components + MealDetail

--- MealsApp/components/MealItem.js
+++ MealsApp/components/MealItem.js
@@ -1,5 +1,6 @@
 import { View, Text, Pressable, Image, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import MealDetail from "./MealDetails";
 
 
 function MealItem({id, title, imageUrl, duration, complexity, affordability}) {
@@ -23,11 +24,11 @@ function MealItem({id, title, imageUrl, duration, complexity, affordability}) {
                         <Image source={{ uri: imageUrl }} style={styles.image} />
                         <Text style={styles.title}>{title}</Text>
                     </View>
-                    <View style={styles.details}>
-                        <Text style={styles.detailItem}>{duration}m</Text>
-                        <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-                        <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-                    </View>
+                    <MealDetail 
+                        duration={duration} 
+                        affordability={affordability}
+                        complexity={complexity}
+                    />
                 </View>
             </Pressable>
         </View>
@@ -65,14 +66,5 @@ const styles = StyleSheet.create({
         fontSize: 18,
         margin: 8
     },
-    details: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 8
-    },
-    detailItem: {
-        marginHorizontal: 4,
-        fontSize: 12
-    }
+    
 });
