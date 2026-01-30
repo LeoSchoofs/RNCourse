1b01f04e47bd4659cb2b115b807633a52949b5c5
Léo Schoofs <leo.schoofs5@gmail.com>
2026-01-30 18:43:46 +0100

    Section 8, 146 : Main App finished

--- ExpenseTrackerApp/screens/AllExpenses.js
+++ ExpenseTrackerApp/screens/AllExpenses.js
@@ -1,7 +1,16 @@
-import { Text } from 'react-native'
+import { useContext } from 'react';
+import ExpensesOutput from '../components/ExpensesOutput/ExpensesOutput';
+import { ExpensesContext } from '../store/expenses-context';
 
 function AllExpenses() {
-    return <Text>AllExpenses Screen</Text>
+    const expensesContext = useContext(ExpensesContext);
+    return (
+        <ExpensesOutput 
+            expenses={expensesContext.expenses} 
+            expensesPeriod="Total"
+            fallbackText="No registered expenses found!"
+        />
+    );
 }
 
 export default AllExpenses;
